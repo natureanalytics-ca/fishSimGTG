0d32f1a2164159c077370389edbcbcbe12264e82
natureanalytics-ca <bill@natureanalytics.ca>
2021-10-29 10:12:24 -0400

    debug LH_to_dataframe function

--- R/utilities.R
+++ R/utilities.R
@@ -24,30 +24,29 @@ LH_to_dataframe <- function(LifeHistoryObj, digits=3) {
   lst <- lapply(nms, function(nm) slot(LifeHistoryObj, nm))
   ind<-which(lengths(lst)!=0)
   data.frame(setNames(lst[ind], nms[ind])) %>%
-    mutate(across(c(Linf, K, t0, L50, L95, M, MK, LW_B, Steep), round, digits))  %>%
-    mutate(across(c(Tmax), round, 1))  %>%
-    mutate(across(c(LW_A), signif, digits))  %>%
+    mutate(across(any_of(c("Linf", "K", "t0", "L50", "L95", "M", "MK", "LW_B", "Steep")), round, digits))  %>%
+    mutate(across(any_of(c("Tmax")), round, 1))  %>%
+    mutate(across(any_of(c("LW_A")), signif, digits))  %>%
     gather() %>%
-    mutate_at("key", str_replace, "title", "Title") %>%
-    mutate_at("key", str_replace, "speciesName", "Species") %>%
-    mutate_at("key", str_replace, "shortDescription", "Short description") %>%
-    mutate_at("key", str_replace, "L_type", "Length type") %>%
-    mutate_at("key", str_replace, "L_units", "Length units") %>%
-    mutate_at("key", str_replace, "Walpha_units", "Weight units") %>%
-    mutate_at("key", str_replace, "Linf", "von Bertalanffy Loo") %>%
-    mutate_at("key", str_replace, "K", "von Bertalanffy K") %>%
-    mutate_at("key", str_replace, "t0", "von Bertalanffy t0") %>%
-    mutate_at("key", str_replace, "L50", "Length at 50% maturity") %>%
-    mutate_at("key", str_replace, "L95", "Length at 95% maturity") %>%
-    mutate_at("key", str_replace, "M", "Natural mortality") %>%
-    mutate_at("key", str_replace, "MK", "M/K") %>%
-    mutate_at("key", str_replace, "LW_A", "Length-weight alpha") %>%
-    mutate_at("key", str_replace, "LW_B", "Length-weight beta") %>%
-    mutate_at("key", str_replace, "Tmax", "Maximum age") %>%
-    mutate_at("key", str_replace, "Steep", "Beverton-Holt steepness") %>%
-    mutate_at("key", str_replace, "author", "Author") %>%
-    mutate_at("key", str_replace, "authAffiliation", "Author affiliation") %>%
-    mutate_at("key", str_replace, "longDescription", "Long description")
+    mutate(across(any_of(c("key")), str_replace, "title", "Title")) %>%
+    mutate(across(any_of(c("key")), str_replace, "speciesName", "Species")) %>%
+    mutate(across(any_of(c("key")), str_replace, "shortDescription", "Short description")) %>%
+    mutate(across(any_of(c("key")), str_replace, "L_type", "Length type")) %>%
+    mutate(across(any_of(c("key")), str_replace, "L_units", "Length units")) %>%
+    mutate(across(any_of(c("key")), str_replace, "Walpha_units", "Weight units")) %>%
+    mutate(across(any_of(c("key")), str_replace, "Linf", "von Bertalanffy Loo")) %>%
+    mutate(across(any_of(c("key")), str_replace, "K", "von Bertalanffy K")) %>%
+    mutate(across(any_of(c("key")), str_replace, "t0", "von Bertalanffy t0")) %>%
+    mutate(across(any_of(c("key")), str_replace, "L50", "Length at 50% maturity")) %>%
+    mutate(across(any_of(c("key")), str_replace, "L95", "Length at 95% maturity")) %>%
+    mutate(across(any_of(c("key")), str_replace, "M", "Natural mortality")) %>%
+    mutate(across(any_of(c("key")), str_replace, "MK", "M/K")) %>%
+    mutate(across(any_of(c("key")), str_replace, "LW_A", "Length-weight alpha")) %>%
+    mutate(across(any_of(c("key")), str_replace, "LW_B", "Length-weight beta")) %>%
+    mutate(across(any_of(c("key")), str_replace, "Tmax", "Maximum age")) %>%
+    mutate(across(any_of(c("key")), str_replace, "Steep", "Beverton-Holt steepness")) %>%
+    mutate(across(any_of(c("key")),str_replace, "author", "Author")) %>%
+    mutate(across(any_of(c("key")), str_replace, "authAffiliation", "Author affiliation"))
 }
 
 
